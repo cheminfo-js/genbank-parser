ea7e72922a3973263e06fa055a93b0cc723d1ec8
Daniel Kostro <kostro.d@gmail.com>
2018-05-17 08:37:25 +0200

    move test

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const parser = require('./index');
+const parser = require('../src/index');
 
 const genbank = fs.readFileSync(
   path.join(__dirname, '../fixtures/gen1.gb'),
